badacb65b03192378406ce29cefb3ea648862211
Patrick Johnson <paddyjohnsonmusic@gmail.com>
2021-04-28 18:17:53 +0100

    classes made for front end logos

--- portfolio/src/Skills.js
+++ portfolio/src/Skills.js
@@ -3,7 +3,14 @@ import React from 'react'
 const Skills = () => {
 
   return (
-    <h1>Look at all this stuff I can do!</h1>
+    <>
+      <h1>{'Technologies I\'ve Used'}</h1>
+      <ul>
+        <h2>Front end</h2>
+        <li><div className="react skill"></div><p>React Hooks</p></li>
+        <li><div className="javascript skill"></div><p>JavaScript</p></li>
+      </ul>
+    </>
   )
 }
 
