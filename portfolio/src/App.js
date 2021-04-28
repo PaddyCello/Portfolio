ba9b8c247686498333172de35e7e7fac85ecca56
Patrick Johnson <paddyjohnsonmusic@gmail.com>
2021-04-28 14:23:06 +0100

    have routes in app.js

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,10 +1,38 @@
 import React from 'react'
 import MyNavbar from './MyNavbar'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import Home from './Home'
+import Skills from './Skills'
+import Projects from './Projects'
+import Interests from './Interests'
+import Contact from './Contact'
 
 const App = () => {
 
   return (
-    <MyNavbar />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path = '/'>
+          <Home />
+        </Route>
+        <>
+          <MyNavbar>
+            <Route path = '/skills'>
+              <Skills />
+            </Route>
+            <Route path = '/projects'>
+              <Projects />
+            </Route>
+            <Route path = '/interests'>
+              <Interests />
+            </Route>
+            <Route path = '/contact'>
+              <Contact />
+            </Route>
+          </MyNavbar>
+        </>
+      </Switch>
+    </BrowserRouter>
   )
 }
 
