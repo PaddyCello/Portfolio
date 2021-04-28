29ed91f82d2cd74889772f8aa62f3d0b63f65cbc
Patrick Johnson <paddyjohnsonmusic@gmail.com>
2021-04-28 22:04:16 +0100

    positioned skill icons and text

--- portfolio/src/Skills.js
+++ portfolio/src/Skills.js
@@ -4,9 +4,9 @@ const Skills = () => {
 
   return (
     <>
-      <h1>{'Technologies I\'ve Used'}</h1>
+      <h1>{'Technologies I\'ve Used:'}</h1>
+      <h2>Front End</h2>
       <ul>
-        <h2>Front end</h2>
         <li><div className="react skill"></div><p>React Hooks</p></li>
         <li><div className="javascript skill"></div><p>JavaScript</p></li>
         <li><div className="html skill"></div><p>HTML</p></li>
@@ -16,8 +16,8 @@ const Skills = () => {
         <li><div className="sass skill"></div><p>Sass</p></li>
         <li><div className="bulma skill"></div><p>Bulma</p></li>
       </ul>
+      <h2>Back End</h2>
       <ul>
-        <h2>Back end</h2>
         <li><div className="express skill"></div><p>Express</p></li>
         <li><div className="node skill"></div><p>Node</p></li>
         <li><div className="python skill"></div><p>Python</p></li>
@@ -25,14 +25,14 @@ const Skills = () => {
         <li><div className="django skill"></div><p>Django</p></li>
         <li><div className="ruby skill"></div><p>Ruby</p></li>
       </ul>
+      <h2>Databases</h2>
       <ul>
-        <h2>Databases</h2>
         <li><div className="mongodb skill"></div><p>MongoDB</p></li>
         <li><div className="postgresql skill"></div><p>PostgreSQL</p></li>
         <li><div className="tableplus skill"></div><p>Table Plus</p></li>
       </ul>
+      <h2>Version Control</h2>
       <ul>
-        <h2>Version Control</h2>
         <li><div className="github skill"></div><p>Git and GitHub</p></li>
       </ul>
     </>
