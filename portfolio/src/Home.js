4e6a58b4df7dc302de451c7f838d27400b3a4ac1
Patrick Johnson <paddyjohnsonmusic@gmail.com>
2021-04-28 14:31:31 +0100

    have navbar links and enter button

--- portfolio/src/Home.js
+++ portfolio/src/Home.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import { Button } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
 
   return (
-    <h1>Look, a Home page!</h1>
+    <>
+      <h1>Look, a Home page!</h1>
+      <Link to='/skills'>
+        <Button variant="success">In you go</Button>{' '}
+      </Link>
+    </>
   )
 }
 
--- portfolio/src/MyNavbar.js
+++ portfolio/src/MyNavbar.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import { Navbar, Nav } from 'react-bootstrap'
 
 const MyNavbar = () => {
 
   return (
-    <h1>Look, a navbar!</h1>
+    <Navbar bg="light" expand="lg">
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link href="/">About</Nav.Link>
+          <Nav.Link href="/skills">Skills</Nav.Link>
+          <Nav.Link href="/projects">Projects</Nav.Link>
+          <Nav.Link href="/contact">Contact</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar> 
   )
 }
 
