45f3e8d9622acccf2bb1c586f7127cfd6814f83a
Patrick Johnson <paddyjohnsonmusic@gmail.com>
2021-04-29 21:51:12 +0100

    more icons and background colour

--- portfolio/src/Skills.js
+++ portfolio/src/Skills.js
@@ -25,16 +25,19 @@ const Skills = () => {
         <li><div className="django skill"></div><p>Django</p></li>
         <li><div className="ruby skill"></div><p>Ruby</p></li>
       </ul>
-      <h2>Databases</h2>
+      <h2>Databases (inc Toolings)</h2>
       <ul>
         <li><div className="mongodb skill"></div><p>MongoDB</p></li>
         <li><div className="postgresql skill"></div><p>PostgreSQL</p></li>
         <li><div className="insomnia skill"></div><p>Insomnia</p></li>
         <li><div className="tableplus skill"></div><p>Table Plus</p></li>
+        <li><div className="sql skill"></div><p>SQL</p></li>
       </ul>
-      <h2>Version Control</h2>
+      <h2>Version Control and Deployment</h2>
       <ul>
         <li><div className="github skill"></div><p>Git and GitHub</p></li>
+        <li><div className="netlify skill"></div><p>Netlify</p></li>
+        <li><div className="heroku skill"></div><p>Heroku</p></li>
       </ul>
     </>
   )
