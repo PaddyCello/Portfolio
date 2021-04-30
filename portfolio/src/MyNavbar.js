b75af621b4e0ec9055b31fde7774558164f1960f
Patrick Johnson <paddyjohnsonmusic@gmail.com>
2021-04-30 09:36:08 +0100

    navbar styles

--- portfolio/src/MyNavbar.js
+++ portfolio/src/MyNavbar.js
@@ -4,15 +4,15 @@ import { Navbar, Nav } from 'react-bootstrap'
 const MyNavbar = () => {
 
   return (
-    <Navbar bg="light" expand="lg">
+    <Navbar id="navbar">
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
-          <Nav.Link href="/">About</Nav.Link>
-          <Nav.Link href="/skills">Skills</Nav.Link>
-          <Nav.Link href="/projects">Projects</Nav.Link>
-          <Nav.Link href="/interests">Interests</Nav.Link>
-          <Nav.Link href="/contact">Contact</Nav.Link>
+          <Navbar.Brand href="/">About</Navbar.Brand>
+          <Navbar.Brand href="/skills">Skills</Navbar.Brand>
+          <Navbar.Brand href="/projects">Projects</Navbar.Brand>
+          <Navbar.Brand href="/interests">Interests</Navbar.Brand>
+          <Navbar.Brand href="/contact">Contact</Navbar.Brand>
         </Nav>
       </Navbar.Collapse>
     </Navbar> 
