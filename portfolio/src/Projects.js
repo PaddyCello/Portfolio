591081d2ee1bb6d93c5b783d3d9ede9667b4e897
Patrick Johnson <paddyjohnsonmusic@gmail.com>
2021-05-05 14:14:17 +0100

    box shadows and project blurbs added

--- portfolio/src/Projects.js
+++ portfolio/src/Projects.js
@@ -52,21 +52,30 @@ const Projects = () => {
       {!corn
         ? <> </>
         : <div className="blurb-holder">
-          <a href="http://cornhubapp.herokuapp.com/" target="blank">CornHub [README]</a>
-          <p className="project-blurb">As a group of three, we built a full-stack app with a React front end and a Python / Django / PostgreSQL back end, over the course of a week. Our app was focused on providing detailed, specific, accessible and searchable information for keen gardeners, complete with many additional features, such as CRUD functionality, authentication, reminders, PayPal integration and weather information. We tackled the majority of the challenging tasks throughout the stack as a group, saving the lesser tasks for individual work. The original concept for this project was mine - I wrote the majority of the pseudocode pre-sign off and assembled the team.</p>
+          <a href="http://cornhubapp.herokuapp.com/" target="blank"><h2>CornHub [README]</h2></a>
+          <p className="project-blurb">As a group of three, we built a full-stack app with a <strong>React</strong> front end and a <strong>Python / Django / PostgreSQL</strong> back end, over the course of a week. Our app was focused on providing detailed, specific, accessible and searchable information for keen gardeners, complete with many additional features, such as CRUD functionality, authentication, reminders, PayPal integration and weather information. We tackled the majority of the challenging tasks throughout the stack as a group, saving the lesser tasks for individual work. The original concept for this project was mine - I wrote the majority of the pseudocode pre-sign off and assembled the team.</p>
         </div>
       }
       {!ice
         ? <> </>
-        : <p>Info on Icelander</p>
+        : <div className="blurb-holder">
+          <a href="https://icelander.netlify.app/" target="blank"><h2>Icelander [README]</h2></a>
+          <p className="project-blurb">As a group of four, we built a full-stack <strong>MongoDB / Express / React / Node</strong> app (complete with custom API) with CRUD functionality over the course of a week, focused on helping users plan road trips around Iceland.  We all worked full-stack, but my main focus was on building much of the back end functionality for the API requests, and much of the full-stack functionality for authentication and saving locations to the user profile.</p>
+        </div>
       }
       {!worth
         ? <> </>
-        : <p>Info on Is It Worth It?</p>
+        : <div className="blurb-holder">
+          <a href="https://isitworthit-hb.netlify.app/" target="blank"><h2>Is It Worth It? [README]</h2></a>
+          <p className="project-blurb">48-hour hackathon. As a pair, we built out a <strong>React</strong> app using two third-party APIs, <a href="https://restcountries.eu/" target="blank">RESTCountries</a> and <a href="https://travelbriefing.org/api" target="blank">TravelBriefing.org</a>. Our app offers travel advice to users attempting to decide where to take their first post-lockdown holiday. This project was a great introduction to the finer points of pair programming, and multi-developer version control.</p>
+        </div>
       }
       {!tetris
         ? <> </>
-        : <p>Info on Tetris</p>
+        : <div className="blurb-holder">
+          <a href="https://paddycello.github.io/SEI-Project-One/" target="blank"><h2>Soviet Tetris [README]</h2></a>
+          <p className="project-blurb">A one-week solo project, building a grid-based game in vanilla <strong>JavaScript</strong> without recourse to HTML canvas. This was a great opportunity to stretch my existing JavaScript skills, as well as expanding my palate of skills with <strong>CSS</strong> animations and <strong>HTML</strong> audio.</p>
+        </div>
       }
     </>
   )
