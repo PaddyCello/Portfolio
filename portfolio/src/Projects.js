79224f9e370c0a3fe3dd3da432ad10b075f70dc7
Patrick Johnson <paddyjohnsonmusic@gmail.com>
2021-05-05 22:58:07 +0100

    readmes in and skills and projects animated

--- portfolio/src/Projects.js
+++ portfolio/src/Projects.js
@@ -34,7 +34,7 @@ const Projects = () => {
     <>
       <h1>Some of my projects - click photo for more info:</h1>
       <Container className="carousel-container">
-        <Carousel fade className="carousel">
+        <Carousel fade className="carousel animate__animated animate__zoomIn animate__slower">
           <Carousel.Item onClick={handleCorn}>
             <div className="cornhub project-pic" />
           </Carousel.Item>
@@ -51,29 +51,33 @@ const Projects = () => {
       </Container>
       {!corn
         ? <> </>
-        : <div className="blurb-holder">
-          <a href="http://cornhubapp.herokuapp.com/" target="blank"><h2>CornHub [README]</h2></a>
+        : <div className="blurb-holder animate__animated animate__slideInDown">
+          <a href="http://cornhubapp.herokuapp.com/" target="blank"><h2>CornHub</h2></a>
+          <a href="https://github.com/PaddyCello/CornHub/blob/master/README.md" target="blank">[README]</a>
           <p className="project-blurb">As a group of three, we built a full-stack app with a <strong>React</strong> front end and a <strong>Python / Django / PostgreSQL</strong> back end, over the course of a week. Our app was focused on providing detailed, specific, accessible and searchable information for keen gardeners, complete with many additional features, such as CRUD functionality, authentication, reminders, PayPal integration and weather information. We tackled the majority of the challenging tasks throughout the stack as a group, saving the lesser tasks for individual work. The original concept for this project was mine - I wrote the majority of the pseudocode pre-sign off and assembled the team.</p>
         </div>
       }
       {!ice
         ? <> </>
-        : <div className="blurb-holder">
-          <a href="https://icelander.netlify.app/" target="blank"><h2>Icelander [README]</h2></a>
+        : <div className="blurb-holder animate__animated animate__slideInDown">
+          <a href="https://icelander.netlify.app/" target="blank"><h2>Icelander</h2></a>
+          <a href="https://github.com/PaddyCello/SEI-Project-Three/blob/main/README.md" target="blank">[README]</a>
           <p className="project-blurb">As a group of four, we built a full-stack <strong>MongoDB / Express / React / Node</strong> app (complete with custom API) with CRUD functionality over the course of a week, focused on helping users plan road trips around Iceland.  We all worked full-stack, but my main focus was on building much of the back end functionality for the API requests, and much of the full-stack functionality for authentication and saving locations to the user profile.</p>
         </div>
       }
       {!worth
         ? <> </>
-        : <div className="blurb-holder">
-          <a href="https://isitworthit-hb.netlify.app/" target="blank"><h2>Is It Worth It? [README]</h2></a>
+        : <div className="blurb-holder animate__animated animate__slideInDown">
+          <a href="https://isitworthit-hb.netlify.app/" target="blank"><h2>Is It Worth It?</h2></a>
+          <a href="https://github.com/PaddyCello/IsItWorthIt/blob/master/README.md" target="blank">[README]</a>
           <p className="project-blurb">48-hour hackathon. As a pair, we built out a <strong>React</strong> app using two third-party APIs, <a href="https://restcountries.eu/" target="blank">RESTCountries</a> and <a href="https://travelbriefing.org/api" target="blank">TravelBriefing.org</a>. Our app offers travel advice to users attempting to decide where to take their first post-lockdown holiday. This project was a great introduction to the finer points of pair programming, and multi-developer version control.</p>
         </div>
       }
       {!tetris
         ? <> </>
-        : <div className="blurb-holder">
-          <a href="https://paddycello.github.io/SEI-Project-One/" target="blank"><h2>Soviet Tetris [README]</h2></a>
+        : <div className="blurb-holder animate__animated animate__slideInDown">
+          <a href="https://paddycello.github.io/SEI-Project-One/" target="blank"><h2>Soviet Tetris</h2></a>
+          <a href="https://github.com/PaddyCello/SEI-Project-One/blob/main/README.md" target="blank">[README]</a>
           <p className="project-blurb">A one-week solo project, building a grid-based game in vanilla <strong>JavaScript</strong> without recourse to HTML canvas. This was a great opportunity to stretch my existing JavaScript skills, as well as expanding my palate of skills with <strong>CSS</strong> animations and <strong>HTML</strong> audio.</p>
         </div>
       }
--- portfolio/src/Skills.js
+++ portfolio/src/Skills.js
@@ -6,7 +6,7 @@ const Skills = () => {
     <>
       <h1>{'Technologies I\'ve Used:'}</h1>
       <h2>Front End</h2>
-      <ul>
+      <ul className="animate__animated animate__fadeInLeft">
         <li><div className="react skill"></div><p>React Hooks</p></li>
         <li><div className="javascript skill"></div><p>JavaScript</p></li>
         <li><div className="html skill"></div><p>HTML</p></li>
@@ -17,7 +17,7 @@ const Skills = () => {
         <li><div className="bulma skill"></div><p>Bulma</p></li>
       </ul>
       <h2>Back End</h2>
-      <ul>
+      <ul className="animate__animated animate__fadeInRight">
         <li><div className="express skill"></div><p>Express</p></li>
         <li><div className="node skill"></div><p>Node</p></li>
         <li><div className="python skill"></div><p>Python</p></li>
@@ -26,7 +26,7 @@ const Skills = () => {
         <li><div className="ruby skill"></div><p>Ruby</p></li>
       </ul>
       <h2>Databases (inc Toolings)</h2>
-      <ul>
+      <ul className="animate__animated animate__fadeInLeft">
         <li><div className="mongodb skill"></div><p>MongoDB</p></li>
         <li><div className="postgresql skill"></div><p>PostgreSQL</p></li>
         <li><div className="insomnia skill"></div><p>Insomnia</p></li>
@@ -34,7 +34,7 @@ const Skills = () => {
         <li><div className="sql skill"></div><p>SQL</p></li>
       </ul>
       <h2>Version Control and Deployment</h2>
-      <ul>
+      <ul className="animate__animated animate__fadeInRight">
         <li><div className="github skill"></div><p>Git and GitHub</p></li>
         <li><div className="netlify skill"></div><p>Netlify</p></li>
         <li><div className="heroku skill"></div><p>Heroku</p></li>
