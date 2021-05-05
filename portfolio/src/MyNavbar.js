f5a0a6161f16b110a30c6106f734ec69b2e7b542
Patrick Johnson <paddyjohnsonmusic@gmail.com>
2021-05-05 14:28:52 +0100

    useLocation makes conditional render work on navbar

--- portfolio/src/MyNavbar.js
+++ portfolio/src/MyNavbar.js
@@ -1,7 +1,10 @@
 import React from 'react'
 import { Navbar, Nav } from 'react-bootstrap'
+import { useLocation } from 'react-router-dom'
 
 const MyNavbar = () => {
+  const { pathname } = useLocation()
+  console.log(pathname)
 
   return (
     <Navbar id="navbar">
@@ -9,10 +12,22 @@ const MyNavbar = () => {
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mx-auto">
           <Navbar.Brand href="/">About</Navbar.Brand>
-          <Navbar.Brand href="/skills">Skills</Navbar.Brand>
-          <Navbar.Brand href="/projects">Projects</Navbar.Brand>
-          <Navbar.Brand href="/interests">Interests</Navbar.Brand>
-          <Navbar.Brand href="/contact">Contact</Navbar.Brand>
+          {pathname === '/skills'
+            ? <></>
+            : <Navbar.Brand href="/skills">Skills</Navbar.Brand>
+          }
+          {pathname === '/projects'
+            ? <></>
+            : <Navbar.Brand href="/projects">Projects</Navbar.Brand> 
+          }
+          {pathname === '/interests'
+            ? <></>
+            : <Navbar.Brand href="/interests">Interests</Navbar.Brand>
+          }
+          {pathname === '/contact'
+            ? <></>
+            : <Navbar.Brand href="/contact">Contact</Navbar.Brand> 
+          }
         </Nav>
       </Navbar.Collapse>
     </Navbar> 
