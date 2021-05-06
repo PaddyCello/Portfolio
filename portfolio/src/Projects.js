92d8b2c3b12f3f177a5cf5892452da2e52117347
Patrick Johnson <paddyjohnsonmusic@gmail.com>
2021-05-06 09:27:15 +0100

    resized and reweighted fonts and added box shadows

--- portfolio/src/Projects.js
+++ portfolio/src/Projects.js
@@ -53,7 +53,7 @@ const Projects = () => {
         ? <> </>
         : <div className="blurb-holder animate__animated animate__slideInDown">
           <a href="http://cornhubapp.herokuapp.com/" target="blank"><h2>CornHub</h2></a>
-          <a href="https://github.com/PaddyCello/CornHub/blob/master/README.md" target="blank">[README]</a>
+          <a href="https://github.com/PaddyCello/CornHub/blob/master/README.md" target="blank"><h6>[README]</h6></a>
           <p className="project-blurb">As a group of three, we built a full-stack app with a <strong>React</strong> front end and a <strong>Python / Django / PostgreSQL</strong> back end, over the course of a week. Our app was focused on providing detailed, specific, accessible and searchable information for keen gardeners, complete with many additional features, such as CRUD functionality, authentication, reminders, PayPal integration and weather information. We tackled the majority of the challenging tasks throughout the stack as a group, saving the lesser tasks for individual work. The original concept for this project was mine - I wrote the majority of the pseudocode pre-sign off and assembled the team.</p>
         </div>
       }
@@ -61,7 +61,7 @@ const Projects = () => {
         ? <> </>
         : <div className="blurb-holder animate__animated animate__slideInDown">
           <a href="https://icelander.netlify.app/" target="blank"><h2>Icelander</h2></a>
-          <a href="https://github.com/PaddyCello/SEI-Project-Three/blob/main/README.md" target="blank">[README]</a>
+          <a href="https://github.com/PaddyCello/SEI-Project-Three/blob/main/README.md" target="blank"><h6>[README]</h6></a>
           <p className="project-blurb">As a group of four, we built a full-stack <strong>MongoDB / Express / React / Node</strong> app (complete with custom API) with CRUD functionality over the course of a week, focused on helping users plan road trips around Iceland.  We all worked full-stack, but my main focus was on building much of the back end functionality for the API requests, and much of the full-stack functionality for authentication and saving locations to the user profile.</p>
         </div>
       }
@@ -69,7 +69,7 @@ const Projects = () => {
         ? <> </>
         : <div className="blurb-holder animate__animated animate__slideInDown">
           <a href="https://isitworthit-hb.netlify.app/" target="blank"><h2>Is It Worth It?</h2></a>
-          <a href="https://github.com/PaddyCello/IsItWorthIt/blob/master/README.md" target="blank">[README]</a>
+          <a href="https://github.com/PaddyCello/IsItWorthIt/blob/master/README.md" target="blank"><h6>[README]</h6></a>
           <p className="project-blurb">48-hour hackathon. As a pair, we built out a <strong>React</strong> app using two third-party APIs, <a href="https://restcountries.eu/" target="blank">RESTCountries</a> and <a href="https://travelbriefing.org/api" target="blank">TravelBriefing.org</a>. Our app offers travel advice to users attempting to decide where to take their first post-lockdown holiday. This project was a great introduction to the finer points of pair programming, and multi-developer version control.</p>
         </div>
       }
@@ -77,7 +77,7 @@ const Projects = () => {
         ? <> </>
         : <div className="blurb-holder animate__animated animate__slideInDown">
           <a href="https://paddycello.github.io/SEI-Project-One/" target="blank"><h2>Soviet Tetris</h2></a>
-          <a href="https://github.com/PaddyCello/SEI-Project-One/blob/main/README.md" target="blank">[README]</a>
+          <a href="https://github.com/PaddyCello/SEI-Project-One/blob/main/README.md" target="blank"><h6>[README]</h6></a>
           <p className="project-blurb">A one-week solo project, building a grid-based game in vanilla <strong>JavaScript</strong> without recourse to HTML canvas. This was a great opportunity to stretch my existing JavaScript skills, as well as expanding my palate of skills with <strong>CSS</strong> animations and <strong>HTML</strong> audio.</p>
         </div>
       }
