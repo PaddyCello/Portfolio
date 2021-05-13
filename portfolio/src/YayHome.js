a2f901c51c27dad23180bac7aee8ee61968970d2
Patrick Johnson <paddyjohnsonmusic@gmail.com>
2021-05-13 22:37:50 +0100

    provisional hypnotic background

--- portfolio/src/YayHome.js
+++ portfolio/src/YayHome.js
@@ -5,12 +5,12 @@ import { Button } from 'react-bootstrap'
 const YayHome = () => {
 
   return (
-    <>
+    <div className="yay-container">
       <h1 className="yay">Look, so crazy!</h1>
       <Link to="/">
         <Button className="yay">Sensible Mode</Button>
       </Link>
-    </>
+    </div>
   )
 }
 
